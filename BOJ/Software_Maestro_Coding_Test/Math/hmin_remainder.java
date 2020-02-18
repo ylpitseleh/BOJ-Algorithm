dc84a8c66fd8176952a93eada524965e2f1d60e0
Hyuk Min <judicious210@gmail.com>
2020-02-18 15:35:49 +0900

    Crt> BOJ folder> ...> hmin_remainder.java

---
+++ BOJ/Software_Maestro_Coding_Test/Math/hmin_remainder.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class hmin_remainder {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
