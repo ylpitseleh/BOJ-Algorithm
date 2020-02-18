657e755b3d3026581b7a8630864d6d0743c2eabf
Yunju Lee <50346189+ylpitseleh@users.noreply.github.com>
2020-02-19 00:43:29 +0900

    Update yl_골드바흐의_추측.java

--- BOJ/SW_Coding_Test_Prep/Math/6588_골드바흐의 추측/yl_골드바흐의_추측.java
+++ BOJ/SW_Coding_Test_Prep/Math/6588_골드바흐의 추측/yl_골드바흐의_추측.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 //에라토스테네스의 체 사용 - 제일 효율적인 방법
-//n보다 작은 소수를 ArrayList에 모두 넣어 반복문이 돌때마다 새로 갱신했더니 50%에서 시간초과가 발생했ㅕ다.
+//n보다 작은 소수를 ArrayList에 모두 넣어 반복문이 돌때마다 새로 갱신했더니 50%에서 시간초과가 발생했다.
 public class 골드바흐의_추측 {
 	public static final int MAX = 1000000;
 	public static void main(String[] args) throws IOException {
