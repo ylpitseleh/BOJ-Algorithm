e9460e2192832e36545bc5914dadea8a1f67aab3
ylpitseleh <lyla02217@gmail.com>
2020-04-20 00:03:40 +0900

    Modify yl 14226

--- src/Graph_and_BFS/이모티콘_14226/yl_이모티콘.java
+++ src/Graph_and_BFS/이모티콘_14226/yl_이모티콘.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-// 너모 어려움. 다시 풀어보기. 이 방법 절대 못 생각해내겠음.
+// 이 방법 절대 못 생각해내겠음. 다시 풀어보기.
 // visited를 2차원 배열로 만드는게 관건.
 public class yl_이모티콘 {
 	static int S;
@@ -14,12 +14,10 @@ public class yl_이모티콘 {
 	static Queue<Emoticon> q = new LinkedList<>();
 	static class Emoticon {
 		int display;
-		boolean copyFlag;
 		int clipBoard;
 		int n;
-		Emoticon (int display, boolean copyFlag, int clipBoard, int n) {
+		Emoticon (int display, int clipBoard, int n) {
 			this.display = display;
-			this.copyFlag = copyFlag; //복사한 상태면 true, 복사한게 없으면 false
 			this.clipBoard = clipBoard; 
 			this.n = n; //필요한 시간 (초)
 		}
@@ -30,7 +28,7 @@ public class yl_이모티콘 {
 		S = Integer.parseInt(br.readLine());
 		visited = new boolean[1001][1001];
 		
-		q.add(new Emoticon(1, false, 0, 0));
+		q.add(new Emoticon(1, 0, 0));
 		// 화면에 있는 이모티콘 개수가 같아도 클립보드에 몇 개 있냐에 따라 달라지므로 visited를 2차원 배열로 만들어야 함.
 		// display * clipBoard
 		visited[S][0] = true;
@@ -44,16 +42,14 @@ public class yl_이모티콘 {
 			
 			for (int i=0; i<3; i++) {
 				int nDisplay = p.display;
-				boolean nCopyFlag = p.copyFlag;
 				int nClipBoard = p.clipBoard;
 				// 화면 모두 복사
 				if (i == 0) {
-					nCopyFlag = true;
 					nClipBoard = nDisplay; // 덮어쓰기
 				}
 				// 클립보드를 화면에 붙여넣기
 				else if (i == 1) {
-					if(!nCopyFlag)
+					if(nClipBoard == 0)
 						continue ;
 					nDisplay += nClipBoard;
 				}
@@ -66,9 +62,8 @@ public class yl_이모티콘 {
 				if (visited[nDisplay][nClipBoard])
 					continue ;
 				visited[nDisplay][nClipBoard] = true;
-				q.add(new Emoticon(nDisplay, nCopyFlag, nClipBoard, p.n + 1));
+				q.add(new Emoticon(nDisplay, nClipBoard, p.n + 1));
 			}
 		}
 	}
-
 }
