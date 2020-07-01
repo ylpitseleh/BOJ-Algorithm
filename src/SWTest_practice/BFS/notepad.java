7dd4f2602f8a6ab7428c2de1be7f64ae80600b70
ylpitseleh <lyla02217@gmail.com>
2020-07-01 18:37:27 +0900

    Add yl 9376

--- src/SWTest_practice/BFS/notepad.java
+++ src/SWTest_practice/BFS/notepad.java
@@ -3,89 +3,161 @@ package SWTest_practice.BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class notepad {
-	static Queue<Integer> q = new LinkedList<>();
-	static int T, A, B;
-	static int[] map;
-	static char[] how;
-	static int[] from;
-	static boolean[] visited;
+    static class Pos {
+        int x, y, door;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		while(T-->0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			
-			map = new int[10001];
-			how = new char[10001];
-			from = new int[10001];
-			visited = new boolean[10001];
-			
-			q.add(A);
-			visited[A] = true;
-			from[A] = -1;
-			
-			while(!q.isEmpty()) {
-				int cur = q.poll();
-				//D
-				int nx = cur * 2;
-				if(nx>9999)
-					nx %= 10000;
-				if(visited[nx]==false) {
-					q.add(nx);
-					visited[nx] = true;
-					map[nx] = map[cur] + 1;
-					from[nx] = cur;
-					how[nx] = 'D';
-				}
-				//S
-				nx = cur - 1;
-				if(nx==-1)
-					nx = 9999;
-				if(visited[nx]==false) {
-					q.add(nx);
-					visited[nx] = true;
-					map[nx] = map[cur] + 1;
-					from[nx] = cur;
-					how[nx] = 'S';
-				}
-				
-				//L
-				nx = (cur%1000)*10 + cur/1000; 
-				if(visited[nx]==false) {
-					q.add(nx);
-					visited[nx] = true;
-					map[nx] = map[cur] + 1;
-					from[nx] = cur;
-					how[nx] = 'L';
-				}
-				
-				//R
-				nx = (cur/10) + 1000*(cur%10);
-				if(visited[nx]==false) {
-					q.add(nx);
-					visited[nx] = true;
-					map[nx] = map[cur] + 1;
-					from[nx] = cur;
-					how[nx] = 'R';
-				}
-				
+        public Pos(int x, int y, int door) {
+            super();
+            this.x = x;
+            this.y = y;
+            this.door = door;
+        }
+
+        @Override
+        public String toString() {
+            return "Pos [x=" + x + ", y=" + y + ", door=" + door + "]";
+        }
+
+    }
+
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
+    static char[][] map = new char[105][105];
+    static int h, w;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken()) + 2;
+            w = Integer.parseInt(st.nextToken()) + 2;
+
+            List<Pos> player = new ArrayList<notepad.Pos>();
+            player.add(new Pos(0, 0, 0));
+            for (int i = 1; i < h - 1; i++) {
+                map[i][0] = '.';
+                map[i][h - 1] = '.';
+                String input = br.readLine();
+                for (int j = 1; j < w - 1; j++) {
+                    map[i][j] = input.charAt(j - 1);
+                    // System.out.println(i+", "+j);
+                    // System.out.println(Arrays.toString(map[i]));
+                    if (map[i][j] == '$') {
+                        player.add(new Pos(i, j, 0));
+                        System.out.println("here : "+i+" "+j);
+                    }
+                }
+
+            }
+
+            for (int j = 0; j < w; j++) {
+                map[0][j] = map[h - 1][j] = '.';
+            }
+			/*
+			 * for (int i=0; i<h; i++) { for (int j=0; j<w; j++) {
+			 * System.out.print(map[i][j]); } System.out.println(); }
+			 */
+            int[][] dist1 = bfs(player.get(0));
+			/*
+			 * for (int i=0; i<h; i++) { for (int j=0; j<w; j++) {
+			 * System.out.print(dist1[i][j]+" "); } System.out.println(); }
+			 * System.out.println();
+			 */int[][] dist2 = bfs(player.get(1));
+			/*
+			 * for (int i=0; i<h; i++) { for (int j=0; j<w; j++) {
+			 * System.out.print(dist2[i][j]+" "); } System.out.println(); }
+			 * System.out.println();
+			 */int[][] dist3 = bfs(player.get(2));
+			/*
+			 * for (int i=0; i<h; i++) { for (int j=0; j<w; j++) {
+			 * System.out.print(dist3[i][j]+" "); } System.out.println(); }
+			 * System.out.println();
+			 */
+            int ans = Integer.MAX_VALUE;
+            int sum = 0;
+            int res[][] = new int[h+2][w+2];
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == '*')
+                        continue;
+                    sum = dist1[i][j] + dist2[i][j] + dist3[i][j];
+                    //if (map[i][j] == '#')
+                       // sum -= 2;
+                    if (ans > sum)
+                    	System.out.println(sum+" "+i+j);
+                    ans = Math.min(ans, sum);
+                    res[i][j] = sum;
+                }
+            }
+			/*
+			 * for (int i=0; i<h; i++) { for (int j=0; j<w; j++) {
+			 * System.out.print(res[i][j]+" "); } System.out.println(); }
+			 */
+            //System.out.print("ans : "+ans + "\n");
+            //System.out.println();
+        }
+    }
+
+    static int[][] bfs(Pos start) {
+        int[][] visit = new int[h][w]; // 몇 개의 문을 열었는지 기록
+        Queue<Pos> q = new LinkedList<Pos>();
+        memset(visit, -1);
+        visit[start.x][start.y] = 0;
+        q.offer(start);
+        
+        System.out.println("== bfs == ");
+		System.out.println(start.y+" "+start.x);
+		for (int i=0; i<h; i++) {
+			for (int j=0; j<w; j++) {
+				System.out.print(visit[i][j]);
 			}
-			StringBuilder ans = new StringBuilder();
-			while(B!=A) { //중요
-				ans.append(how[B]);
-				B = from[B];
-			}
-			System.out.println(ans.reverse());
+			System.out.println();
 		}
-		
-	}
-	
+		System.out.println("==========");
+        
+
+        while (!q.isEmpty()) {
+            Pos now = q.poll();
+            int x = now.x;
+            int y = now.y;
+            int door = now.door;
+            //System.out.println(x + ", " + y);
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                int ndoor = door;
+                if (nx < 0 || nx > h - 1 || ny < 0 || ny > w - 1)
+                    continue;
+                if (map[nx][ny] == '*')
+                    continue;
+                if (map[nx][ny] == '#') { // 문일 경우
+                    ndoor++;
+                }
+                if (visit[nx][ny] == -1 || visit[nx][ny] > ndoor) {
+                    visit[nx][ny] = ndoor;
+                    q.offer(new Pos(nx, ny, ndoor));
+                }
+            }
+        }
+
+        return visit;
+    }
+
+    static void memset(int[][] arr, int val) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                arr[i][j] = val;
+            }
+        }
+    }
 }
--- src/SWTest_practice/BFS/탈옥_9376/yl_탈옥.java
+++ src/SWTest_practice/BFS/탈옥_9376/yl_탈옥.java
@@ -3,31 +3,51 @@ package SWTest_practice.BFS.탈옥_9376;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
-
+/*
+ * https://stack07142.tistory.com/145 -> 아이디어 참고
+ * https://shinyou1024.tistory.com/48 -> 소스 참고
+ * 아이디어만 있으면 충분히 구현할 수 있는 수준인데 아이디어가 문제임 이걸 어떻게 생각해 내... 
+ * 죽었다 깨어나도 못 생각해내지 이건
+ */
 public class yl_탈옥 {
-	//static Queue q = new LinkedList<Pos>();
-	
+	static int h, w;
 	static int dy[] = {-1, 1, 0, 0};
 	static int dx[] = {0, 0, -1, 1};
 	static char map[][];
-	static boolean visited[][];
+	static class Pos {
+		int y;
+		int x;
+		int door;
+		
+		Pos (int y, int x, int door) {
+			this.y = y;
+			this.x = x;
+			this.door = door;
+		}
+	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		while (T-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int h = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
 			map = new char[h+2][w+2];
-			visited = new boolean[h+2][w+2];
+			ArrayList<Pos> al = new ArrayList<Pos>();
+			// 상근이부터 시작하는 경우
+			al.add(new Pos(0, 0, 0));
 			for (int i=1; i<h+1; i++) {
 				String s = br.readLine();
 				for (int j=1; j<w+1; j++) {
 					map[i][j] = s.charAt(j-1);
+					// 죄수 1, 2부터 시작하는 경우
+					if (map[i][j] == '$')
+						al.add(new Pos(i, j, 0));
 				}
 			}
 			for (int i=0; i<h+2; i++) {
@@ -37,13 +57,66 @@ public class yl_탈옥 {
 				}
 			}
 			
+			int[][] minDoor1 = bfs(al.get(0));
+			int[][] minDoor2 = bfs(al.get(1));
+			int[][] minDoor3 = bfs(al.get(2));
 			
+			// 여기서 왜 sum 중에 최소값이 답인지 이해가 안 감.
+			int sum = 0;
+			int ans = Integer.MAX_VALUE;
+			int res[][] = new int[h+2][w+2];
+			for (int i=0; i<h+2; i++) {
+				for (int j=0; j<w+2; j++) {
+					if (map[i][j] == '*')
+						continue ;
+					sum = minDoor1[i][j] + minDoor2[i][j] + minDoor3[i][j];
+					// 문이면 상근, 죄수1, 죄수2가 각 한 번씩 총 3번 지나간거니까 -2 해줘야 함.
+					if (map[i][j] == '#')
+						sum -= 2;
+					ans = Math.min(ans, sum);
+					res[i][j] = sum;
+				}
+			}
 			
-			
-			
+			System.out.println(ans);
 		}
-			
 
 	}
-
+	public static int[][] bfs (Pos start) {
+		int minDoor[][] = new int[h+2][w+2];
+		Queue<Pos> q = new LinkedList<Pos>();
+		
+		minDoor = fillArray(minDoor, -1);
+		q.add(start);
+		minDoor[start.y][start.x] = 0;
+		while (!q.isEmpty()) {
+			Pos p = q.poll();
+			for (int i=0; i<4; i++) {
+				int ny = p.y + dy[i];
+				int nx = p.x + dx[i];
+				int ndoor = p.door;
+				
+				if (ny < 0 || ny >= h+2 || nx < 0 || nx >= w+2)
+					continue ;
+				if (map[ny][nx] == '*')
+					continue ;
+				if (map[ny][nx] == '#')
+					ndoor++;
+				if (minDoor[ny][nx] == -1 || minDoor[ny][nx] > ndoor) {
+					q.add(new Pos(ny, nx, ndoor));
+					minDoor[ny][nx] = ndoor;
+				}
+			}
+		}
+		return minDoor;
+	}
+	
+	public static int[][] fillArray(int[][] arr, int value) {
+		for (int i=0; i<h+2; i++) {
+			for (int j=0; j<w+2; j++) {
+				arr[i][j] = value;
+			}
+		}
+		return arr;
+	}
 }
